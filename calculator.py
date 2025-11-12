da0cd29949785be54bbc94c9a6997145df00c983
luis34-code <unclelmm11@gmail.com>
2025-11-12 17:13:46 -0500

    Update calculator.py

---
+++ calculator.py
@@ -0,0 +1,34 @@
+import math
+
+def square_root(a):
+    if a < 0:
+        raise ValueError
+    else:
+        return math.sqrt(a)
+
+def hypotenuse(a, b):
+    return math.hypot(a, b)
+
+def add(a, b):
+    return a + b
+
+def subtract(a, b):
+    return a - b
+
+def mul(a, b):
+    return a * b
+
+def div(a, b):
+    if a == 0:
+        raise ZeroDivisionError
+    else:
+        return b / a
+
+def log(a, b):
+    if b <= 0:
+        raise ValueError
+    return math.log(b, a)
+
+def exp(a, b):
+    return a ** b
+
